ffbfd8262cedfdfc927d370bfb54722d6ee93c5e
kadour <kadourzaroui399@gmail.com>
2025-10-22 08:27:15 -0400

    connect to postgres DB

--- marketplace_backend/settings.py
+++ marketplace_backend/settings.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -64,12 +64,17 @@ WSGI_APPLICATION = 'marketplace_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
-
-
+print(os.getenv("NAME"))
+print(os.getenv("USER"))
+print(os.getenv("PASSWORD"))
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
